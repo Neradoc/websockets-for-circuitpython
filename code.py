8e1cc437ba253120bb860253c14c30231968d50c
Neradoc <neraOnGit@ri1.fr>
2021-01-30 14:25:40 +0100

    test script with debug False, small changes

--- code.py
+++ code.py
@@ -1,3 +1,7 @@
+import micropython
+# set debug to False for future imports
+micropython.opt_level(1)
+
 import time
 import sys
 import json
@@ -7,20 +11,23 @@ import socketpool
 import ssl
 import secrets
 
+print("CONNECT WIFI")
 wifi.radio.connect(secrets.ssid,secrets.password)
 pool = socketpool.SocketPool(wifi.radio)
 ssl_context = ssl.create_default_context()
 
-print("connected with SSL ready ?")
-
 import uwebsockets.client
 
+print("TESTING SSL WEBSOCKETS")
 url = "wss://echo.websocket.org"
 ws = uwebsockets.client.connect(url)
 
 message = "Repeat this"
+print(f"SENDING:  <{message}>")
 ws.send(message)
 result = ws.recv()
-print("RESULT",result)
+print(f"RECEIVED: <{result}>")
 if result == message:
-	print("SUCCESS")
+	print("SUCCESS !!")
+else:
+	print("OH NO IT WENT WRONG")
