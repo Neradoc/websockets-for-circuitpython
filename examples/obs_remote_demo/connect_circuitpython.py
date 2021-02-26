dd2b81bb1d891171b18cdd01a6fbca15bf7198ca
Neradoc <neraOnGit@ri1.fr>
2021-02-26 05:51:50 +0100

    more checks and comments in the connect_circuitpython script

--- examples/obs_remote_demo/connect_circuitpython.py
+++ examples/obs_remote_demo/connect_circuitpython.py
@@ -8,7 +8,9 @@ def connect_wifi():
 		native_wifi = False
 	
 	if native_wifi:
-		#import wifi
+		"""
+		That's for native wifi.
+		"""
 		import socketpool
 		import ssl
 
@@ -19,14 +21,25 @@ def connect_wifi():
 		return (socket, ssl_context, None)
 	
 	else:
+		"""
+		That's for an ESP32 wifi coprocessor (airlift breakouts and integrated).
+		Change the pins to those that match your board.
+		"""
 		import board
 		import busio
 		from digitalio import DigitalInOut
 		from adafruit_esp32spi import adafruit_esp32spi
 
-		esp32_cs = DigitalInOut(board.D13)
-		esp32_ready = DigitalInOut(board.D11)
-		esp32_reset = DigitalInOut(board.D12)
+		if hasattr(board,"ESP_CS"):
+			# If you are using a board with pre-defined ESP32 Pins:
+			esp32_cs = DigitalInOut(board.ESP_CS)
+			esp32_ready = DigitalInOut(board.ESP_BUSY)
+			esp32_reset = DigitalInOut(board.ESP_RESET)
+		else:
+			# If you have an externally connected ESP32:
+			esp32_cs = DigitalInOut(board.D13)    # CHANGE TO MATCH YOUR BOARD
+			esp32_ready = DigitalInOut(board.D11) # CHANGE TO MATCH YOUR BOARD
+			esp32_reset = DigitalInOut(board.D12) # CHANGE TO MATCH YOUR BOARD
 
 		spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 		esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
