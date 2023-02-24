fdcf7def903da14672f14eb8d867df567c195e70
Neradoc <neraOnGit@ri1.fr>
2023-02-24 17:54:37 +0100

    Add simple echo server for tests

---
+++ examples/cpython_server/echo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import os
+import logging
+
+
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.DEBUG,
+)
+
+PORT = 5000
+
+async def echo(websocket, path):
+    try:
+        async for message in websocket:
+            print ("Received and echoing message: "+message, flush=True)
+            await websocket.send(message)
+    except:
+        print("Bye")
+
+start_server = websockets.serve(echo, "0.0.0.0", PORT)
+
+print("WebSockets echo server starting", flush=True)
+asyncio.get_event_loop().run_until_complete(start_server)
+
+print("WebSockets echo server running", flush=True)
+asyncio.get_event_loop().run_forever()
